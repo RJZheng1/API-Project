a81573d0d3dfff82fe37093834915eb893bba6e5
shishu1998 <shishu_1998@yahoo.com>
2015-11-10 12:27:45 -0500

    Update getstuff.py

--- getstuff.py
+++ getstuff.py
@@ -3,10 +3,11 @@ import json
 
 #API here 
 url = "https://en.wikipedia.org/w/api.php?action=query&titles=Germany&prop=revisions&rvprop=content&format=json"
-
+#loading stuff here
 request = urllib2.urlopen(url)
 result = request.read()
 r = json.loads(result)
+#finding the keys
 print r.keys()
 print r[u'query'].keys()
 print r[u'query'][u'pages'].keys()
