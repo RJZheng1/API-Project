ab599ab44f0e172a79138df4502c1106c09ec84e
RJZheng1 <stuykid2016@yahoo.com>
2015-11-11 17:22:24 -0500

    Splits the text into paragraphs.

--- getstuff.py
+++ getstuff.py
@@ -4,11 +4,17 @@ from bs4 import BeautifulSoup
 
 #API here
 def getAPI(name):
-    url = "https://en.wikipedia.org/w/api.php?action=query&titles={}&prop=revisions&rvprop=content&format=json".format(name)
+    url = "https://en.wikipedia.org/w/api.php?action=query&titles={}&prop=extracts&rvprop=content&format=json".format(name)
     result = json.loads(urllib2.urlopen(url).read())
     key = result['query']['pages'].keys()[0]
-    info = result[u'query']['pages'][key]['revisions'][0]
-    soup = BeautifulSoup(info['*'],'html.parser')
-    print soup
-    
+    info = result[u'query']['pages'][key]['extract']
+    soup = BeautifulSoup(info, 'html.parser')
+    paragraphs = []
+    for p in soup.find_all('p'):
+        paragraphs.append(p.get_text().encode("ascii","ignore"))
+    for i in paragraphs:
+        print i
+        
 getAPI("Germany")
+
+
