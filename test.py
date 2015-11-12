fc85331026a48d4a48a2c6e114f3f446e88631ab
ivan <ivlin112@gmail.com>
2015-11-11 21:14:16 -0500

    playing w google places

--- test.py
+++ test.py
@@ -1,18 +1,30 @@
 import urllib2, json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+placesKey = "AIzaSyCX2pUzMG4Es5RjILnPwBQ8RG1kn0855BI"
+
 @app.route("/")
 def main():
+    query = "gemsny"
     url = "https://en.wikipedia.org/w/api.php?action=query&titles=%s&prop=revisions&rvprop=content&format=json"
-    url = url%"Germany"
+   
+    url = url%query
+
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&key=%s"%(query,placesKey)
+
     request = urllib2.urlopen(url)
+
     result = request.read()
-    r = json.loads(result)
-    print r['query']['pages']['11867']['title']
-    return result
+
+    print type(result)
+    print result
+
+    return json.load(request.read())
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
