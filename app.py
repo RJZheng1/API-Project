54a9b5b9ed61832c47fe6c4a9514787ce89b7a3a
Shi Shu <shishu_1998@yahoo.com>
2015-11-12 21:56:42 -0500

    stuff

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask,render_template
-from flask import redirect,url_for
+from flask import Flask,render_template,redirect,request
 import getstuff
 
 app = Flask(__name__)
 @app.route("/",methods = ["GET","POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+         
+if __name__ == "__main__":
+    app.debug = True
+    app.secret_key = "Idk"
+    app.run(host = "0.0.0.0", port = 4000)
